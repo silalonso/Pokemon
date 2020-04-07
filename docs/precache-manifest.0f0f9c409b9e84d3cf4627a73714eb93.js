c9866ce7d73aa668940619ec9a1cc7f8ba0b42d1
silvia <silvia.alonso.pozas@gmail.com>
2020-04-07 12:06:59 +0200

    Add docs

---
+++ docs/precache-manifest.0f0f9c409b9e84d3cf4627a73714eb93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9506013e8872b21fc7294d8734234b9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b0c118bb1038852aeb62",
+    "url": "./static/css/main.7c421f3c.chunk.css"
+  },
+  {
+    "revision": "6997b46d3612d1dd6cf7",
+    "url": "./static/js/2.b5003f3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b5003f3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0c118bb1038852aeb62",
+    "url": "./static/js/main.7d9b36de.chunk.js"
+  },
+  {
+    "revision": "e6d65b8c83558982e9a5",
+    "url": "./static/js/runtime-main.4416497a.js"
+  }
+]);
