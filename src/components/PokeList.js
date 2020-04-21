6b5aba3bd740b95644ae1a20652b64920f42d07d
silvia <silvia.alonso.pozas@gmail.com>
2020-04-21 19:47:38 +0200

    Adding handleClick lifting

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,12 +1,19 @@
-import React from 'react';
+// import React from 'react';
 import '..//stylesheets/App.css';
+import React, { useState } from 'react';
 import Pokemon from './Pokemon.js';
 
 const PokeList = (props) => {
+  const [disabled, setEnabled] = useState(true);
+  const handleClickEnPL = () => {
+    console.log('me han clicado en pokelist');
+    setEnabled(!disabled);
+  };
+  const clicked = disabled ? '' : 'enabled';
   const pokemonLis = props.pokemons.map((pokemon) => {
     return (
       <li key={pokemon.id}>
-        <Pokemon pokemon={pokemon} />
+        <Pokemon pokemon={pokemon} handleClick={handleClickEnPL} />
       </li>
     );
   });
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,11 +2,17 @@ import React, { useState } from 'react';
 import '..//stylesheets/App.css';
 
 const Pokemon = (props) => {
+  console.log(props.handleClick);
+  console.log('me han clicado en POKEMON');
   const [disabled, setEnabled] = useState(true);
   const clicked = disabled ? '' : 'enabled';
+  // const handleClick = () => {
+  //   setEnabled(!disabled);
+  // };
+  props.handleClick();
 
   return (
-    <div className='card' id={clicked} onClick={() => setEnabled(!disabled)}>
+    <div className='card' id={clicked} onClick={props.handleClick}>
       <img src={props.pokemon.url} alt={props.name} />
       <p className='name'>{props.pokemon.name}</p>
       <ul>
